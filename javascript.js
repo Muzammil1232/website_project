bff8c4676d05067cdd8a9cb098d573ccc9c2e34d
Muzammil1232 <muzammil844803@gmail.com>
2025-01-24 20:24:22 -0800

    first commit

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// function hide(){
+//     let click=document.getElementsByClassName('click');
+//     let elements=document.getElementsByClassName('elements');
+//     if(elements.style.display != 'none'){
+//         elements.style.display='none';
+//     }
+//     else{
+//         elements.style.display='block';
+//     }
+// }
