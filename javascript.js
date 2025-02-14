cb94de90836fe3753cca130a635e035fadad7ec0
Muzammil1232 <muzammil844803@gmail.com>
2025-02-14 05:31:34 -0800

    changes-3

--- javascript.js
+++ javascript.js
@@ -1,78 +1,13 @@
-// function hide(){
-//     let click=document.getElementsByClassName('click');
-//     let elements=document.getElementsByClassName('elements');
-//     if(elements.style.display != 'none'){
-//         elements.style.display='none';
-//     }
-//     else{
-//         elements.style.display='block';
-//     }
-// }
 
+//nav-bar to be displayed in mobile 
+const menuIcon=document.getElementById('menu-icon');
+const navLinks=document.getElementById('nav-links');
 
-// function search_courses(){
-//     let input = document.getElementById('searchbar').value;
-//     input = input.toLowerCase();
-//     let x=document.getElementsByClassName('course');
-//     for(i=0; i<x.length; i++)
-//     {
-//         if(!x[i].innerHTML.toLowerCase().includes(input))
-//         {
-//             x[i].style.display="none"
-//         }
-//         else{
-//             x[i].style.display="list-items"
-//         }
-//     }
-// }
-
-
-// function searchFunction() {
-//     let input = document.getElementById('search-input').value.toLowerCase();
-//     let items = document.querySelectorAll('#search-results li');
-   
-//     items.forEach(function(item) {
-//         if (item.textContent.toLowerCase().includes(input)) {
-//             item.classList.remove('hidden');
-//         } else {
-//             item.classList.add('hidden');
-//         }
-//     });
-// }
-
-// function clearSearch() {
-//     document.getElementById('search-input').value = '';
-//     searchFunction();
-// }
-
-
-// // Function to show the list when the input field is focused
-// function showList() {
-//     document.getElementById('itemList').classList.remove('hidden');
-//   }
-  
-//   // Function to filter items based on input
-//   function filterItems() {
-//     let input = document.getElementById('searchInput').value.toLowerCase();
-//     let items = document.getElementById('itemList').getElementsByTagName('li');
-
-//     for (let i = 0; i < items.length; i++) {
-//       let itemText = items[i].textContent.toLowerCase();
-//       if (itemText.indexOf(input) > -1) {
-//         items[i].style.display = '';
-//       } else {
-//         items[i].style.display = 'none';
-//       }
-//     }
-//   }
-
-//   // Function to handle the search button click (optional)
-//   function searchItems() {
-//     let inputValue = document.getElementById('searchInput').value;
-//     alert('You searched for: ' + inputValue);
-//   }
-
-
+menuIcon.addEventListener('click', ()=>
+{
+  navLinks.classList.toggle('active');
+});
+//search bar 
 function showSuggestions() {
   document.getElementById('suggestionsList').style.display = 'block';
 }
